d66fab10eeefbbe2cd95bc016fefd309179bd6a7
Mindaugas Kniazevas <mindaugas.kniazevas@gmail.com>
2016-07-01 14:02:14 +0200

    Update

---
+++ C5.Tests/concurrent/heaps/ConcurrentHeapTests.cs
@@ -0,0 +1,36 @@
+﻿/*
+ Copyright (c) 2003-2016 <...insert names here...>
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
+
+using System;
+using C5;
+using C5.concurrent;
+using NUnit.Framework;
+
+namespace C5UnitTests.concurrent.heaps
+{
+    using CollectionOfInt = C5.concurrent.IntervalHeap<int>;
+    [TestFixture(MemoryType.Normal)]
+    [TestFixture(MemoryType.Strict)]
+    [TestFixture(MemoryType.Safe)]
+    class ConcurrentHeapTests
+    {
+    }
+}
