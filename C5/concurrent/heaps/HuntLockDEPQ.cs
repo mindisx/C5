b2cd4a849abf30106376ae279592283fd5a8dc2a
Mindaugas Kniazevas <mindaugas.kniazevas@gmail.com>
2016-10-04 12:01:02 +0200

    Structure for Hunt's priority queue

---
+++ C5/concurrent/heaps/HuntLockDEPQ.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using SCG = System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C5.concurrent.heaps
+{
+    class HuntLockDEPQ<T> : IConcurrentPriorityQueue<T>
+    {
+        struct Interval
+        {
+            internal Node first, last;
+
+            public override string ToString()
+            {
+                return string.Format("[{0}; {1}]", first.ToString(), last.ToString());
+            }
+        }
+
+        struct Node
+        {
+            internal object lockObject;
+            internal T element;
+            internal int tag;
+            public override string ToString()
+            {
+                return string.Format("[{0}", element);
+            }
+        }
+
+        SCG.IComparer<T> comparer;
+        SCG.IEqualityComparer<T> itemEquelityComparer;
+        Interval[] heap;
+        int size;
+
+        public HuntLockDEPQ() : this(16) { }
+
+        public HuntLockDEPQ(int capacity)
+        {
+            this.comparer = SCG.Comparer<T>.Default;
+            this.itemEquelityComparer = SCG.EqualityComparer<T>.Default;
+            int lenght = 1;
+            while (lenght < capacity)  lenght <<= 1; //lenght is always equal to 2 power by some number. 
+            heap = new Interval[lenght];
+        }
+
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SCG.IEnumerable<T> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Check()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T DeleteMax()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T DeleteMin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindMax()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindMin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
